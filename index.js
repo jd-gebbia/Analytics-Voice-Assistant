8a2d18298bdf9aed80b1ca48b9f3b7d6273fe9d1
gfvandehei <gfvandehei@gmail.com>
2018-03-07 20:09:08 -0500

    added a basic test case for post requests

--- index.js
+++ index.js
@@ -16,31 +16,12 @@ server.use(bodyParser.json());
 //create server and define arsing srategies
 
 server.post('/make-request', function (req, res) {
-    
-        let resuest_info = req.body.result && req.body.result.parameters &&
-         req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-        let reqUrl = encodeURI('http://theapache64.xyz:8080/movie_db/search?keyword=' + request_info);
-        http.get(reqUrl, (responseFromAPI) => {
-    
-            responseFromAPI.on('data', function (chunk) {
-                let movie = JSON.parse(chunk)['data'];
-                let dataToSend = movieToSearch === 'The Godfather' ? 'I don\'t have the required info on that. Here\'s some info on \'The Godfather\' instead.\n' : '';
-                dataToSend += movie.name + ' is a ' + movie.stars + ' starer ' + movie.genre + ' movie, released in ' + movie.year + '. It was directed by ' + movie.director;
-    
-                return res.json({
-                    speech: dataToSend,
-                    displayText: dataToSend,
-                    source: 'make-request'
-                });
-            });
-        }, (error) => {
-            return res.json({
-                speech: 'Something went wrong!',
-                displayText: 'Something went wrong!',
-                source: 'make-request'
-            });
-        });
+    return res.json({
+        speech: 'this is a test',
+        displayText: 'Test has been completed if you see this',
+        source: '/make-request'
     });
+});
 //define a route on the server.js
 
 server.listen((process.env.PORT || 8000), function () {
